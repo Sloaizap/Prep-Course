04e017395d91b56ed97f9e8d120e9717c0682461
SantiagoL <sloaiza.2001@gmail.com>
2022-07-30 22:39:16 -0500

    homework cambio 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -76,7 +76,8 @@ function tienenMismaLongitud(str1, str2) {
   // Tu código:
   if (str1.lenght == str2.lenght){
     return true;
-   } else {
+  } 
+  else {
     return false;
   }
   }
@@ -180,9 +181,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (num > 0){
+  if (numero > 0){
     return 'Es positivo';
-  } else if (num < 0){
+  } else if (numero < 0){
     return 'Es negativo';
   } else{
     return false;
@@ -246,7 +247,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if (letra.toLowerCase in ['a','e','i','o','u'] && letra.lenght==1 ){
+
+
+if (letra.length > 1){
+  return "Dato incorrecto";
+}
+const vocales = ['a','e','i','o','u']
+
+for (i in vocales){
+  if (vocales[i]===letra){
     return 'Es vocal';
   
   } else{
